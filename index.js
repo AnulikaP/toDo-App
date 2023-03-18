766a0a4eac14ed23596586d1ce17d2858028b50e
AnulikaP <anulinnedu@gmail.com>
2023-03-18 22:06:53 +0100

    second commit

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 
-
 const inputHere = document.querySelector('#writeHere');
 const addNow = document.querySelector('#addHere');
 const listGroup = document.querySelector('.list');
 const whatToDo = document.querySelector('.listItems');
 
-whatToDo.innerHTML = localStorage.getItem('value');
+
+
+whatToDo.innerHTML = localStorage.getItem('textInput');
 
 function display() {
-    localStorage.setItem('myStorage', input.value);
-    
+    localStorage.setItem('textInput', inputHere.value);
+        
 }
+
+inputHere.addEventListener('input', letter => {
+    whatToDo.textContent = letter.target.value;
+});
+
 
 addNow.addEventListener('click', (e) => {
     e.preventDefault();
+
+ const slowDown = inputHere.value;
+ if (!slowDown) {
+   return alert('Please input a Todo');
+    
+}
  //   You can add a new item to the todo list (required)
 
  if(inputHere.value === "") {
     console.log("empty")
  }
  else {
-    const newListItem = document.createElement('li');
+ const newListItem = document.createElement('li');
  const newBtn = document.createElement('button');
+ const editbtn = document.createElement('button');
 
- newBtn.textContent = "DEL";
- newBtn.style.background = 'red';
- newBtn.style.border = 'none'
+ newBtn.textContent = "Del";
+
+ editbtn.textContent = 'Edit';
 
  newBtn.className = "delBtn";
- 
+ editbtn.className = 'editButton';
 
  newListItem.className = 'listItems';
 
@@ -36,8 +49,8 @@ addNow.addEventListener('click', (e) => {
 const newListText = document.createTextNode(inputHere.value);
 
 newListItem.appendChild(newListText);
-newListItem.appendChild(newBtn)
-
+newListItem.appendChild(newBtn);
+newListItem.appendChild(editbtn);
 
 //the todo list must show the most recently added item first on the list
 
@@ -59,6 +72,8 @@ listGroup.addEventListener('click', (e) => {
         listGroup.removeChild(li)
     }
 });
+
+
 
 
 
